f83f4830559542430477b5e0c521fbce6651be13
equal0 <equal0@naver.com>
2016-12-05 17:13:31 +0900

    test

---
+++ 1stProject/src/manager/Interface.java
@@ -0,0 +1,19 @@
+package manager;
+
+import java.util.ArrayList;
+
+import vo.Category;
+import vo.Evaluation;
+import vo.Member;
+import vo.Restaurant;
+
+public interface Interface {
+	public boolean insertRestaurant(Restaurant restaurant);
+	public boolean login(Member member);
+	public boolean join(Member member);
+	public boolean evaluateResataurant(Evaluation evalauation);
+	public ArrayList<Restaurant> showList(Category category, int num);
+	public boolean logout(Member member);
+	public void askRestaurant(Category category, Member member, boolean isRandom);
+	public void replyRestaurant(Category category, Restaurant restaurant, Member to, Member from);
+}
---
+++ 1stProject/src/vo/Restaurant.java
@@ -0,0 +1,70 @@
+package vo;
+
+import java.util.ArrayList;
+
+import javax.swing.ImageIcon;
+
+public class Restaurant {
+	
+	private String restaurantName;
+	private String price;
+	private String operationHour;
+	private Category category;
+	private ArrayList<ImageIcon> images;
+	private ArrayList<Evaluation> userEvaluations;
+	private ArrayList<String> menu;
+	private int state;
+	
+	public static final int STANDBY = 1;
+	public static final int REGISTRATION = 2;
+	public String getRestaurantName() {
+		return restaurantName;
+	}
+	public void setRestaurantName(String restaurantName) {
+		this.restaurantName = restaurantName;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getOperationHour() {
+		return operationHour;
+	}
+	public void setOperationHour(String operationHour) {
+		this.operationHour = operationHour;
+	}
+	public Category getCategory() {
+		return category;
+	}
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+	public ArrayList<ImageIcon> getImages() {
+		return images;
+	}
+	public void setImages(ArrayList<ImageIcon> images) {
+		this.images = images;
+	}
+	public ArrayList<Evaluation> getUserEvaluations() {
+		return userEvaluations;
+	}
+	public void setUserEvaluations(ArrayList<Evaluation> userEvaluations) {
+		this.userEvaluations = userEvaluations;
+	}
+	public ArrayList<String> getMenu() {
+		return menu;
+	}
+	public void setMenu(ArrayList<String> menu) {
+		this.menu = menu;
+	}
+	public int getState() {
+		return state;
+	}
+	public void setState(int state) {
+		this.state = state;
+	}
+	
+	
+}
